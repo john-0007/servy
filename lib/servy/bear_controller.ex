9f15b060d53e763e2f6c6f60a92fb5c119db4964
John Joseph <xohnjoseph@gmail.com>
2022-10-02 02:49:43 +0700

    wip commit

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -2,24 +2,30 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
+  @templates_path Path.expand("templates", File.cwd!())
+
+  defp render(conv, template, bindings \\ []) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %{conv | status: 200, resp_body: content}
   end
 
   def index(conv) do
-    items =
+    bears =
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join()
 
-    %{conv | status: 200, resp_body: "<ul>#{items}</ul>"}
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %{conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>"}
+
+    render(conv, "show.eex", bear: bear)
+
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
